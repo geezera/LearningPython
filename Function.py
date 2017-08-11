6cbe528f78c2080f49bbfcd207f2310110aaaa90
levan.nioradze <pontoneli@gmail.com>
2017-08-11 18:15:40 +0400

    test

---
+++ Function.py
@@ -0,0 +1,11 @@
+list_of_numbers= [1,2,3,4]
+def get_median(list_of_numbers):
+    list_of_numbers.sort()
+    if len(list_of_numbers) % 2 ==1:
+        middle = list_of_numbers[len(list_of_numbers)//2]
+    else:
+        middle = (list_of_numbers[len(list_of_numbers) // 2] + list_of_numbers[len(list_of_numbers) // 2-1 ]) /2
+
+    return middle
+
+print(get_median(list_of_numbers))
