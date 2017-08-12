6ffc52dfd62477cfc4066487c8476953db486165
Levan.Nioradze(Mac) <pontoneli@gmail.com>
2017-08-12 21:44:38 +0400

    deleting Secondary.py (test file)

--- TestDir/Function.py
+++ TestDir/Function.py
@@ -1,5 +1,6 @@
 list_of_numbers= [1,2,3,4]
 def get_median(list_of_numbers):
+    """ფუნქცია, რომელიც ითვლის მითითებული რიცხვების მედიანას"""
     list_of_numbers.sort()
     if len(list_of_numbers) % 2 ==1:
         middle = list_of_numbers[len(list_of_numbers)//2]
@@ -7,5 +8,5 @@ def get_median(list_of_numbers):
         middle = (list_of_numbers[len(list_of_numbers) // 2] + list_of_numbers[len(list_of_numbers) // 2-1 ]) /2
 
     return middle
-print"woalahhh"
+
 print(get_median(list_of_numbers))
